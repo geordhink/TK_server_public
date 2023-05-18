0e2c93f243646a909bc7f43c110a0f22da072bca
Geordhi NKOUNKOU <kaydongeordhi@gmail.com>
2023-05-18 13:31:33 +0200

    Changing som details on settings

--- TekaServer/settings.py
+++ TekaServer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-+fqc=yte3d2q-=dwmt&-fy7x9c)f8*u*7x(^!pf3uff4%xf6*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "geordhink.pythonanywhere.com/",
+    '*',
+]
 
 
 # Application definition
@@ -131,6 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/'
+MEDIA_ROOT = '/'
+# STATIC_URL = '/static/'
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
