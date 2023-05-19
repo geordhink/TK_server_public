90d118089a947ec1e68e5d5991e3f1ac6311fe2b
Geordhi NKOUNKOU <kaydongeordhi@gmail.com>
2023-05-19 14:21:26 +0200

    comment on the settings static root

--- TekaServer/settings.py
+++ TekaServer/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/'
-MEDIA_ROOT = '/'
-# STATIC_URL = '/static/'
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# MEDIA_URL = '/'
+# MEDIA_ROOT = '/'
+# # STATIC_URL = '/static/'
+# STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
